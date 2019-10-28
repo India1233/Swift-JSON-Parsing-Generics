fa766bc6503463016aca79ea1358a490cb72c08d
shigas <shigasuresh546@gmail.com>
2019-10-28 14:10:51 +0530

    Initial Commit

---
+++ Swift JSON Parsing-Generics/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift JSON Parsing-Generics
+//
+//  Created by shiga on 28/10/19.
+//  Copyright © 2019 Shigas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
